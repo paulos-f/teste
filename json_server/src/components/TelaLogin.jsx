e808a86f3c4f0b02b846b83ff988576e1c9a94fd
paulos-f <133170300+paulos-f@users.noreply.github.com>
2024-06-10 20:25:56 -0300

    login e cadastro
    finalizando a tela de login e cadastro de usuario

--- json_server/src/components/TelaLogin.jsx
+++ json_server/src/components/TelaLogin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-function login() {
+function TelaLogin() {
     const  [email, setEmail] = useState('');
     const  [password, setPassword] = useState('');
 
--- json_server/src/components/TelaLoginCadastro.jsx
+++ json_server/src/components/TelaLoginCadastro.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-function cadastroUsuario() {
+function TelaLoginCadastro() {
     const  [username, setUsername] = useState('');
     const  [email, setEmail] = useState('');
     const  [password, setPassword] = useState('');
@@ -22,8 +22,21 @@ function cadastroUsuario() {
                     <label htmlFor="username">Nome de Usuario</label>
                     <input type="text" id='username' value={username} onChange={(e) => setUsername(e.target.value)} required />
                 </div>
-                
+                <div className='input-group'>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id='email' value={email} onChange={(e) => setEmail(e.target.value)} required/>
+                </div>
+                <div className='input-group'>
+                    <label htmlFor="password">Senha</label>
+                    <input type="password" id='password' value={password} onChange={(e) => setPassword(e.target.value)} required/>
+                </div>
+                <div className='input-group'>
+                    <label htmlFor="password">Senha</label>
+                    <input type="password" id='confirm-password' value={confirmpassword} onChange={(e) => setCondirmpassword(e.target.value)} required/>
+                </div>
+                <button type='sumbit' className='register-button'>Cadastrar</button>
             </form>
         </div>
     )
 }
+export default TelaLoginCadastro
