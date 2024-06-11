b738d04e092ef0c0add59d539261b8d5f7aedd20
paulos-f <133170300+paulos-f@users.noreply.github.com>
2024-06-10 21:36:15 -0300

    2

--- json_server/src/routes/NavBar.jsx
+++ json_server/src/routes/NavBar.jsx
@@ -3,9 +3,12 @@ import { Link } from "react-router-dom";
 export default function NavBar(){
     return(
         <div>
-           <span style={{padding: '8px'}}><Link to={'/'}>Home</Link></span>
-           <span style={{padding: '8px'}}><Link to={'/products'}>Produtos</Link></span>
-           <span style={{padding: '8px'}}><Link to={'/miranha'}>Miranha</Link></span>
+           <span style={{padding: '8px'}}><Link to={'/'}>login</Link></span>
+           <span style={{padding: '8px'}}><Link to={'/login'}>teste</Link></span>
+           <span style={{padding: '8px'}}><Link to={'/cadastro'}>cabelo</Link></span>
         </div> 
     )
 }
+
+
+
